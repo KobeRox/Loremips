49f6f759014ce76fdf4f84af6f2064f9480fed7c
Admin <admin@Admins-MacBook-Pro.local>
2019-08-18 16:48:56 +1200

    ss

--- convertDataToHtml.js
+++ convertDataToHtml.js
@@ -1,16 +1,17 @@
 
 function convertDataToHtml(data){
     var html;
+    var test2 = document.getElementById('restoResults');
+    test2.innerHTML = " ";
     for(var test in data){
         let dataRow = data[test]
         var row = document.createElement('tr');
         for (var property in dataRow) {
-            var th = document.createElement('th');
+            var th = document.createElement('td');
             th.scope = "col";
             th.innerText = dataRow[property];
             row.appendChild(th);
         }    
-        var test2 = document.getElementById('restoResults');
         test2.appendChild(row);
     }
     return html;
