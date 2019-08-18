fba757027ede08cb01f368fe7f6b0656f74834a9
Kriti <30788769+krikk20@users.noreply.github.com>
2019-08-18 14:54:11 +1200

    formatted data from Zomato

--- zomato_restaurants.js
+++ zomato_restaurants.js
@@ -1,16 +1,11 @@
-function get_restaurants(cuisine_id){
-	
-	// API call
-	var response;
+function get_restaurants(cuisine_id, callback){
 	$.ajax({
          url: "https://developers.zomato.com/api/v2.1/search?city_id=70&cuisines={cuisine_id}",
          type: "GET",
          beforeSend: function(xhr){xhr.setRequestHeader('user-key', 'b17db59020e7fbe543111e1e6c2e5ecb');},
          success: function(data) {
-			console.log(data);
-			response = data;
+			callback(convertContract(data));
 		 }
 	});
-	
-	// Send data to function
 }
+
