c374e633094619f6ed3f31daa5e439f600a093a5
KobeRox <888888xc@gmail.com>
2019-09-08 22:09:00 +1200

    all changes

---
+++ .history/convertDataToHtml_20190908215725.js
@@ -0,0 +1,17 @@
+function convertDataToHtml(data) {
+  var html;
+  var test2 = document.getElementById("restoResults");
+  //test2.innerHTML = " ";
+  for (var test in data) {
+    let dataRow = data[test];
+    var row = document.createElement("tr");
+    for (var property in dataRow) {
+      var th = document.createElement("td");
+      th.scope = "col";
+      th.innerText = dataRow[property];
+      row.appendChild(th);
+    }
+    test2.appendChild(row);
+  }
+  return html;
+}
---
+++ .history/zomato_restaurants_20190908160631.js
@@ -0,0 +1,11 @@
+function get_restaurants(cuisine_id, callback){
+	$.ajax({
+         url: "https://developers.zomato.com/api/v2.1/search?city_id=70&cuisines=" + cuisine_id,
+         type: "GET",
+         beforeSend: function(xhr){xhr.setRequestHeader('user-key', 'b17db59020e7fbe543111e1e6c2e5ecb');},
+         success: function(data) {
+			callback(convertContract(data));
+		 }
+	});
+}
+
